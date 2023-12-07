78377754724127743236e6b0b4a0e3e845820f53
kolooook <84650747+kolooook@users.noreply.github.com>
2023-12-07 22:01:03 +0800

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,23 +1,45 @@
-let pitch = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
 let roll = 0
+let pitch = 0
+radio.setGroup(0)
 basic.forever(function () {
     pitch = input.rotation(Rotation.Pitch)
     roll = input.rotation(Rotation.Roll)
     if (Math.abs(pitch) < 20 && Math.abs(roll) < 20) {
         basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
             `)
+        radio.sendNumber(0)
     } else {
         basic.showLeds(`
-            # # # # #
-            # # . # #
-            # . # . #
-            # # # # #
-            # # # # #
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
             `)
+        radio.sendNumber(1)
     }
 })
